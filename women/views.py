57a516a1346c281bf0087de85948306c3a64c121
erlixbaxman <beginerdd@gmail.com>
2023-03-21 12:04:09 +0300

    DjangoRest ListCreateAPIView added

--- women/views.py
+++ women/views.py
@@ -1,18 +1,25 @@
 from rest_framework.response import Response
+from rest_framework import generics
 from rest_framework.views import APIView
 from .models import Women
 from .serializers import WomenSerializer
 
 
-class WomenAPIView(APIView):
-    def get(self, request):
-        w = Women.objects.all()
-        return Response({'posts': WomenSerializer(w, many=True).data})
+class WomenAPIList(generics.ListCreateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
 
-    def post(self, request):
-        serializer = WomenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+
+# class WomenAPIView(APIView):
+#     def get(self, request):
+#         w = Women.objects.all()
+#         return Response({'posts': WomenSerializer(w, many=True).data})
+#
+#     def post(self, request):
+#         serializer = WomenSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({'post': serializer.data})
 
     def put(self, request, *args, **kwargs):
         pk = kwargs.get("pk", None)
